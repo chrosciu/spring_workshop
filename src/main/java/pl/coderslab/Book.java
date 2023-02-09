e30847847e7609676bca45376165cffa42dd5923
Marcin Chrost <mchrost82@gmail.com>
2023-02-09 16:54:40 +0100

    Added DB connection + Book changed to entity

--- src/main/java/pl/coderslab/Book.java
+++ src/main/java/pl/coderslab/Book.java
@@ -1,6 +1,16 @@
 package pl.coderslab;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "books")
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String isbn;
     private String title;
